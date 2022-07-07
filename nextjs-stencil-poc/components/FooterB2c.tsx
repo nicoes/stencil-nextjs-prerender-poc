30d80519585c2d889f67ac86c734a715139b7969
neshui <eshuis.nico@gmail.com>
2022-07-07 14:21:35 +0200

    Put style in separate JSX tag

--- nextjs-stencil-poc/components/FooterB2c.tsx
+++ nextjs-stencil-poc/components/FooterB2c.tsx
@@ -5,18 +5,21 @@ defineCustomElement()
 
 const FooterB2c = ({ userName }: { userName: string | null }) => {
     const component = React.createElement("footer-b2c", {}, null)
-
-    return {
+    const DecoratedComponent = {
         ...component,
         props: {
             ...component.props,
             class: 'sc-footer-b2c-h hydrated',
             ['user-name']: userName,
             dangerouslySetInnerHTML: {
-                __html: `<style sty-id="sc-footer-b2c">/*!@footer*/footer.sc-footer-b2c{display:block;min-height:100px;background:#0070f3;color:white;width:100%}</style><!--r.1--><footer class=\"sc-footer-b2c\" c-id=\"1.0.0.0\"><section class=\"sc-footer-b2c\" c-id=\"1.1.1.0\"><!--t.1.2.2.0-->This is the footer</section></footer>`
+                __html: `<!--r.1--><footer class="sc-footer-b2c" c-id="1.0.0.0"><section class="sc-footer-b2c" c-id="1.1.1.0"><!--t.1.2.2.0-->This is the footer</section></footer>`
             }
         }
     }
+    return <React.Fragment key={'footer-b2c'}>
+        <style dangerouslySetInnerHTML={{ __html: '/*!@footer*/footer.sc-footer-b2c{display:block;min-height:100px;background:#0070f3;color:white;width:100%}'}} />
+        {DecoratedComponent}
+    </React.Fragment>
 }
 
 export default FooterB2c
--- nextjs-stencil-poc/components/HeaderB2c.tsx
+++ nextjs-stencil-poc/components/HeaderB2c.tsx
@@ -5,17 +5,20 @@ defineCustomElement()
 
 const HeaderB2c = () => {
     const component = React.createElement("my-component", {}, null)
-
-    return {
+    const DecoratedComponent = {
         ...component,
         props: {
             ...component.props,
             className: 'sc-my-component-h hydrated',
             dangerouslySetInnerHTML: {
-                __html: "<style sty-id=\"sc-my-component\">/*!@:host*/.sc-my-component-h{display:block}/*!@button*/button.sc-my-component{color:#AA00CC}</style><!--r.1--><button c-id=\"1.0.0.0\"><!--t.1.1.1.0-->0</button>"
+                __html: "<!--r.1--><button class=\"sc-my-component\" c-id=\"1.0.0.0\"><!--t.1.1.1.0-->0</button>"
             }
         }
     }
+    return <React.Fragment key={'header-b2c'}>
+        <style dangerouslySetInnerHTML={{ __html: '/*!@:host*/.sc-my-component-h{display:block}/*!@button*/button.sc-my-component{color:#AA00CC}'}} />
+        {DecoratedComponent}
+    </React.Fragment>
 }
 
 export default HeaderB2c
